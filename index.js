d51c6eae044f3bf58cb70aed6f731c742af084cb
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-18 03:09:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = []
 
 function takeANumber(lineOfPeople, name){
   lineOfPeople[lineOfPeople.length] = name
-  return `Welcome, ${name}. You are ${lineOfPeople.length} in line.`
+  return `Welcome, ${name}. You are number ${lineOfPeople.length} in line.`
 }
 
 function nowServing(line){
