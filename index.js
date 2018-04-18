99d9f1c3af524fa0f9bd61b7b356e0623396ab6a
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-18 02:26:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function takeANumber(lineOfPeople, name){
   lineOfPeople[name] = lineOfPeople.length + 1
 }
 
+function nowServing(line){
+  if(line.length == 0)
+  {
+    return "There is nobody waiting to be served!"
+  }
+  return line.shift()
+}
